5ac61b580464589d98b1de5f24cb013dfef6977f
urooj-alyssum <168622496+urooj-alyssum@users.noreply.github.com>
2024-08-28 18:25:37 +0530

    Update latestsci.py

--- latestsci.py
+++ latestsci.py
@@ -6,17 +6,17 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
-# Set up Chrome options
+# Setting up Chrome options
 chrome_options = Options()
 chrome_options.add_experimental_option("prefs", {
     "download.default_directory": os.path.abspath("/Users/uroojkhan/Desktop/selenium/downloaded_pdfs"),  # Update this to your download directory
     "plugins.always_open_pdf_externally": True
 })
 
-# Manually set the path to the ChromeDriver
+# Manually setting the path to the ChromeDriver
 chromedriver_path = "/Users/uroojkhan/Desktop/selenium/chromedriver"  # Update this to the path where you downloaded chromedriver
 
-# Initialize the WebDriver
+# Initializing the WebDriver
 service = Service(chromedriver_path)
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
@@ -29,17 +29,17 @@ driver.get(judgments_page_url)
 # Wait for the page to load
 time.sleep(5)
 
-# Find all the PDF links in the judgments column
+# Finding all the PDF links in the judgments column
 pdf_links = driver.find_elements(By.XPATH, "//a[contains(@href, 'get_court_pdf') and contains(@href, 'type=j&order_date')]")
 
-# Extract the URLs from the links
+# Extracting the URLs from the links
 urls = set(link.get_attribute('href') for link in pdf_links if "latest_judgements_order" in link.get_attribute('href'))  # Filter to include only the 'latest_judgements_order' type
 
-# Create download directory if it doesn't exist
+# Creating download directory if it doesn't exist
 download_dir = "/Users/uroojkhan/Desktop/selenium/downloaded_pdfs"
 os.makedirs(download_dir, exist_ok=True)
 
-# Iterate through the list of URLs and download the PDFs
+# Iterating through the list of URLs and download the PDFs
 for url in urls:
     print(f"Downloading from: {url}")
     driver.get(url)
@@ -47,7 +47,7 @@ for url in urls:
     print(f"Metadata: URL - {url}")
     time.sleep(5)  # Wait for the download to complete (adjust time if needed)
 
-# Close the WebDriver
+# Closing the WebDriver
 driver.quit()
 
 print("All PDFs related to 'latest_judgements_order' have been downloaded to the local directory.")
@@ -57,7 +57,7 @@ s3_bucket_name = "datacrux-dev"
 s3_prefix = "sci/Latest/August/"
 s3 = boto3.client('s3')
 
-# Upload PDFs to S3
+# Upload PDFs to S3 Bucket
 for root, dirs, files in os.walk(download_dir):
     for file in files:
         if file.endswith(".pdf"):
